e2811f43f7058bbeaaacd3c0633a1e5c2347ee94
PrabathMadushan <prabathmadushan5@gmail.com>
2024-11-21 14:40:12 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='bg-slate-600 text-purple-500 text-lg'>This is App</h1>
+    </div>
+  )
+}
+
+export default App
